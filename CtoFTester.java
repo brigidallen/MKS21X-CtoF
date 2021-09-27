e8f3473bc3c47834e2f64a37a8bc69b794d1a114
brigidallen <brigidhallen@gmail.com>
2021-09-27 00:33:19 -0400

    Create CtoFTester.java

---
+++ CtoFTester.java
@@ -0,0 +1,8 @@
+public class CtoFtester{
+  public static void celsiusToFahrenheit(int c) {
+    int f = c * 9 / 5 + 32;
+    System.out.println("Your temperature is " + f + "degrees Fahrenheit");
+  }
+}
+
+//  public static void 
